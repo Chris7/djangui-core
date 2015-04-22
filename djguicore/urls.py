3e9759c109a0b79a74d4abe24d3fd7fcea49e64a
Chris Mitchell <chris.mit7@gmail.com>
2015-04-22 00:29:20 -0400

    auto find apps and add to urls

--- djguicore/urls.py
+++ djguicore/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import include, url
 from .views import celery_status, CeleryTaskView, celery_task_command
 
@@ -6,3 +7,10 @@ urlpatterns = [
     url(r'^celery/status$', celery_status, name='celery_results'),
     url(r'^celery/(?P<task_id>[a-zA-Z0-9\-]+)/$', CeleryTaskView.as_view(), name='celery_results_info'),
 ]
+
+# add urlpatterns of djangui apps
+for app_name, app in getattr(settings, 'DJANGUI_APPS', {}).iteritems():
+    dj_app = app['app']
+    urlpatterns += [
+        url(r'^', include('{0}.urls'.format(dj_app.DJANGUI_NAME)))
+    ]
